de93e622dc7ad8bfec366e64994e8eb563b59c81
Dmitriy <astrovskiy.04@gmail.com>
2023-12-11 18:44:43 +0300

    upd

--- WPF_Course_project/App.xaml.cs
+++ WPF_Course_project/App.xaml.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -13,6 +15,9 @@ namespace WPF_Course_project
     /// </summary>
     public partial class App : Application
     {
+        public static App Me => ((App)Application.Current);
+        public int x = 0;
+        public static HttpClient client = new HttpClient();
         protected override void OnStartup(StartupEventArgs e)
         {
             // Call the base implementation first
@@ -20,7 +25,14 @@ namespace WPF_Course_project
 
             // Add your custom logic here
             // For example, show a custom startup window or perform some initialization tasks
-            int x = 32;
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            x = 32;
+        }
+        public async Task<string> get_json(string url)
+        {
+            HttpResponseMessage response = await client.GetAsync(url);
+            string response_text = await response.Content.ReadAsStringAsync();
+            return response_text;
         }
     }
 
--- WPF_Course_project/MainWindow.xaml.cs
+++ WPF_Course_project/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,9 +22,19 @@ namespace WPF_Course_project
     /// </summary>
     public partial class MainWindow : Window
     {
+        
+        
         public MainWindow()
         {
+            
+            
             InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+            Buttonz.Content = await App.Me.get_json("http://127.0.0.1:8000/api/cryptos/");
         }
     }
 }
